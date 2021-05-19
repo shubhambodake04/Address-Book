9d7bf074b14ea397f78ff2c9e5d078f15b61012a
shubhambodake04 <shubhambodake6476@gmail.com>
2021-05-19 15:40:34 +0530

    Added contacts

---
+++ Address-Book/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book
+{
+    class Contact
+    {
+        public string FirstName, LastName, Address, City, State, Email;
+        public double PhoneNumber, Zip;
+
+        public Contact(string FirstName, string LastName, string Address, string City, string State, double PhoneNumber, double Zip, string Email)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Address = Address;
+            this.City = City;
+            this.State = State;
+            this.PhoneNumber = PhoneNumber;
+            this.Zip = Zip;
+            this.Email = Email;
+
+        }
+
+    }
+}
--- Address-Book/Program.cs
+++ Address-Book/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Address_Book
 {
@@ -6,7 +7,33 @@ namespace Address_Book
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome To AdressBook");
+            Console.WriteLine("<======================>");
+
+            Console.WriteLine("Enter FirstName");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Enter LastName");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            string City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string State = Console.ReadLine();
+            Console.WriteLine("Enter Email");
+            string Email = Console.ReadLine();
+            Console.WriteLine("Enter PhoneNumber");
+            int PhoneNNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Zip");
+            int Zip = Convert.ToInt32(Console.ReadLine());
+
+            ArrayList list = new ArrayList();
+            list.Add(new Contact(FirstName, LastName, Address, City, State, PhoneNNumber, Zip, Email));
+
+            foreach (Contact Info in list)
+            {
+                Console.WriteLine(Info.FirstName + ", " + Info.LastName + ", " + Info.Address + ", " + Info.City + ", " + Info.State + ", " + Info.PhoneNumber + ", " + Info.Zip + ", " + Info.Email);
+            }
         }
     }
 }
